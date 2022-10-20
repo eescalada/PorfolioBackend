942d01043a2207ef97ca7ef9f1cef579bf9f9895
eescalada <87399264+eescalada@users.noreply.github.com>
2022-10-19 22:21:53 -0300

    hys agregada

---
+++ src/main/java/com/porfolioWeb/SpringBoot/service/Shys.java
@@ -0,0 +1,57 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.porfolioWeb.SpringBoot.service;
+
+
+import com.porfolioWeb.SpringBoot.model.hys;
+import com.porfolioWeb.SpringBoot.repository.Rhys;
+import java.util.List;
+import java.util.Optional;
+import javax.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+@Service
+@Transactional
+public class Shys {
+    @Autowired
+    Rhys rhys;
+    
+    public List<hys> list(){
+        return rhys.findAll();
+    }
+    
+    public Optional<hys> getOne(int id){
+        return rhys.findById(id);
+    }
+    
+    public Optional<hys> getByNombre(String nombre){
+        return rhys.findByNombre(nombre);
+    }
+    
+    public void save(hys skill){
+        rhys.save(skill);
+    }
+    
+    public void delete(int id){
+        rhys.deleteById(id);
+    }
+    
+    public boolean existsById(int id){
+        return rhys.existsById(id);
+    }
+    
+    public boolean existsByNombre(String nombre){
+        return  rhys.existsByNombre(nombre);
+    }
+    
+    
+    
+    
+}
+
+    
+
