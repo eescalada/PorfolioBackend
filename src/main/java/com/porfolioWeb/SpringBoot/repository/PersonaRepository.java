3be18087305928df2e74a466eb2983e89fccc0f3
eescalada <87399264+eescalada@users.noreply.github.com>
2022-10-24 11:41:47 -0300

    cambios finales ok

--- src/main/java/com/porfolioWeb/SpringBoot/repository/PersonaRepository.java
+++ src/main/java/com/porfolioWeb/SpringBoot/repository/PersonaRepository.java
@@ -1,7 +1,9 @@
 
 package com.porfolioWeb.SpringBoot.repository;
 
+
 import com.porfolioWeb.SpringBoot.model.Persona;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
